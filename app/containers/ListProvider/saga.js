ad8171235f0e872867c9e87d57212ca9df1be1b2
zacharyhill <zhillb@gmail.com>
2018-07-15 13:52:29 -0700

    Refactor ListProvider saga to use API_URL from config

--- app/containers/ListProvider/saga.js
+++ app/containers/ListProvider/saga.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { put, takeEvery, call } from 'redux-saga/effects';
 import { SET_STRINGS, FETCH } from './constants';
+import { API_URL } from '../../config';
 
 // Individual exports for testing
 export default function* watchFetch() {
@@ -8,7 +9,7 @@ export default function* watchFetch() {
 }
 
 export function* fetch() {
-  const results = yield call(axios.get, 'http://localhost:3000/api/strings');
+  const results = yield call(axios.get, API_URL);
   const { data } = results;
   yield put({ type: SET_STRINGS, payload: data });
 }
